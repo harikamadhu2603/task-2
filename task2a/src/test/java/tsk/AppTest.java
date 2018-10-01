afe4f59f28bf593390bf24361dba27a4c221ab28
root <root@ip-172-31-94-27.ec2.internal>
2018-10-01 15:55:00 +0000

    jenkins task

---
+++ task2a/src/test/java/tsk/AppTest.java
@@ -0,0 +1,20 @@
+package tsk;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
